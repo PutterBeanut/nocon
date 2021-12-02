c1ca6bc1e248bae2d61cab0a3c9eba9a34fd98ad
JxshD <46508425+JxshD@users.noreply.github.com>
2021-12-02 01:41:11 -0600

    Adds features to announce user entry and departure.
    Upon entering, 'user has entered the chat' is broadcasted. The message loop is then running until the user enters '@leave'. When this is done, the user's departure is announced and the loops end.

--- src/com/criticalangle/Main.java
+++ src/com/criticalangle/Main.java
@@ -2,26 +2,41 @@ package com.criticalangle;
 
 import com.criticalangle.connection.Connection;
 
-import javax.swing.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        // Creates connection and scanner.
         Connection connection = new Connection(9527);
-
         Scanner scanner = new Scanner(System.in);
 
+        // Gets username from user.
         System.out.print("Username: ");
         String username = scanner.nextLine();
 
+        // Printed after the username and text to avoid X box error.
+        String whiteSpaces = "                                                         ";
+
+        // Announces the user has entered.
+        connection.sendBroadcast(username + " has entered the chat." + whiteSpaces);
+
+        // While loop for continuous texting.
         while (true) {
+
+            // User's msg.
             String line = scanner.nextLine();
 
-            if (line.equals("@leave"))
-                break;
+            // If the user wants to leave, break the loop.
+            if (line.equals("@leave")) {
 
-            connection.sendBroadcast(username + ": " + line);
+                // Announces user's departure and breaks loop.
+                connection.sendBroadcast(username + " has left the chat." + whiteSpaces);
+                break;
+            }
+
+            // Send the message.
+            connection.sendBroadcast(username + ": " + line + whiteSpaces);
         }
 
         connection.closeReceiver();
