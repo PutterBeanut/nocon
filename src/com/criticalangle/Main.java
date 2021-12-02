8faaa54437b67e8bec37fe497b6c859c77976b8d
PutterBeanut <christian.bannoura@gmail.com>
2021-12-01 23:38:33 -0600

    Added support for multiple messages

--- src/com/criticalangle/Main.java
+++ src/com/criticalangle/Main.java
@@ -2,9 +2,21 @@ package com.criticalangle;
 
 import com.criticalangle.connection.Connection;
 
+import javax.swing.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Connection connection = new Connection(Integer.parseInt(args[0]));
-        connection.sendBroadcast(args[1]);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String line = scanner.nextLine();
+
+            if (line.equals("@leave"))
+                break;
+
+            connection.sendBroadcast(line);
+        }
     }
 }
